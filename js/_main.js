039641beb6953cff9c806d3e99710b708e1b3a6d
Gavin Corbett <gavin.corbett@dowjones.com>
2016-06-29 22:26:10 +0100

    Major refactor & tidy up of code & start on the mobile version

--- js/_main.js
+++ js/_main.js
@@ -9,44 +9,60 @@
  */
 
 ( function( $ ) {
-	
+
 	// Setup variables
 	$window = $(window);
 	$slide = $('.homeSlide');
 	$body = $('body');
-	
-    //FadeIn all sections   
+
+    //FadeIn all sections
 	$body.imagesLoaded( function() {
 		setTimeout(function() {
-		      
+
 		      // Resize sections
 		      adjustWindow();
-		      
+
 		      // Fade in sections
 			  $body.removeClass('loading').addClass('loaded');
-			  
+
 		}, 800);
 	});
-	
+
 	function adjustWindow(){
-		
+
 		// Init Skrollr
-		
-		
+
+
 		// Get window size
 	    winH = $window.height();
-	    
+
 	    // Keep minimum height 550
 	    if(winH <= 550) {
 			winH = 650;
-		} 
-	    
+		}
+
 	    // Resize our slides
 	    $slide.height(winH);
-	    
+
 	    // Refresh Skrollr after resizing our sections
-	    
-	    
+
+
 	}
-		
+
 } )( jQuery );
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
+      return false;
+    }
+  }
+  });
+});
+
