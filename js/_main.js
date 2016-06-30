a902d06a7786dab406212fe68f204f2426b351d2
Gavin Corbett <gavin.corbett@dowjones.com>
2016-06-30 19:50:59 +0100

    Update mobile nav to work correctly, remove unrequired links

--- js/_main.js
+++ js/_main.js
@@ -68,23 +68,6 @@ $(function() {
 
 $(function($){
   $( '.menu-btn' ).click(function(){
-    $('.responsive-menu').addClass('expand')
-      $('.menu-btn').addClass('btn-none')
+    $('.responsive-menu').toggleClass('expand')
   })
-
-  $( '.close-btn' ).click(function(){
-    $('.responsive-menu').removeClass('expand')
-      $('.menu-btn').removeClass('btn-none')
-  })
-})
-$(function($){
-  $( '.menu-btn' ).click(function(){
-    $('.responsive-menu').addClass('expand')
-      $('.menu-btn').addClass('btn-none')
-  })
-
-  $( '.close-btn' ).click(function(){
-    $('.responsive-menu').removeClass('expand')
-      $('.menu-btn').removeClass('btn-none')
-  })
-})
+});
